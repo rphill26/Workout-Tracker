ebc0adae4af421e1d2f6ebe0a97c302e6c2d6a21
Randall Phillips <rphill45@uncc.edu>
2019-12-10 11:09:36 -0500

    setup node modules and mongoose

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
 const mongojs = require("mongojs");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const databaseUrl = "";
-const collections = [""];
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const db = mongojs(databaseUrl, collections);
+app.use(express.static("public"));
+
+// MAKE SURE TO CHANGE THE PATHWAY
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/INSERT-DATABASE-HERE",
+{ useNewUrlParser: true });
+// MAKE SURE TO CHANGE THE PATHWAY
+
+
+// const databaseUrl = "";
+// const collections = [""];
+
+// const db = mongojs(databaseUrl, collections);
 
 db.on("error", error => {
     console.log("Database Error:", error);
 });
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
