b2c7904aa0c9c7d7cba764eabe5e3ee3de96633f
Randall Phillips <rphill45@uncc.edu>
2019-12-11 22:10:16 -0500

    fixing server connection

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://workoutTracker:rootroot1@
 
 // const db = mongojs(databaseUrl, collections);
 
-db.on("error", error => {
-    console.log("Database Error:", error);
-});
+// db.on("error", error => {
+//     console.log("Database Error:", error);
+// });
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
